fb411bb64396a205e605c70c5c2339630fed14ed
AlexMamani85 <alex.mamani@tecsup.edu.pe>
2023-03-16 17:21:44 -0500

    CHORE: start TodoListItem.vue

---
+++ src/components/TodoListItem.vue
@@ -0,0 +1,51 @@
+<script>
+export default {
+  props: {
+    task: {
+      type: Object,
+      required: true,
+      // validator(value) {
+      //   return "Okay" === value;
+      // }
+    },
+    done: Boolean,
+    priority: Boolean,
+    // id: {
+    //   required: false,
+    //   type: Number,
+    //   validator(value) {
+    //     return value >= 1 && value <= 100;
+    //   },
+    //   // default() {
+    //   //   return [];
+    //   // }
+    // }
+  },
+  emits: ["update:done", "update:priority"],
+};
+</script>
+
+<template>
+  <div
+    class="bg-white shadow-sm rounded-md text-gray-700 text-xs md:text-sm p-4"
+    :class="{ 'opacity-25 line-through': task.done }"
+  >
+    <div>{{ task.description }}</div>
+    <div class="py-4 bg-white">
+      <base-checkbox
+        class="mb-2"
+        @update:model-value="$emit('update:done', $event)"
+        :model-value="done"
+        >Done
+      </base-checkbox>
+
+      <base-checkbox
+        @update:model-value="$emit('update:priority', $event)"
+        :model-value="priority"
+        >Prioritized
+      </base-checkbox>
+    </div>
+  </div>
+</template>
+
+<style></style>
